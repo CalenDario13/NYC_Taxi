27fd3f307f2584b2413b6f31896658c2cca9b3ce
CalenDario13 <dariorusso.dr13@gmail.com>
2021-06-23 15:45:35 +0200

    CLOSE_CLUSTER: remove also buckets

--- create_cluster/close_cluster.sh
+++ create_cluster/close_cluster.sh
@@ -1,3 +1,5 @@
 CLUSTER=$1
 
-gcloud container clusters delete $CLUSTER
+gcloud dataproc clusters delete $CLUSTER --region us-central1
+gsutil rm -r gs://dataproc-staging-us-central1-335361100481-glio0kgy
+gsutil rm -r gs://dataproc-temp-us-central1-335361100481-3dzbxcwx
